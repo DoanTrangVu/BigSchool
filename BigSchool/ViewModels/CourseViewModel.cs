341d1635915114908abaec99e95e18eaef38abb7
vu trang <doantrang.0412@5gt54p.onmicrosoft.com>
2023-03-17 22:35:00 +0700

    update 14/3

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -1,16 +1,24 @@
 ﻿using BigSchool.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place {get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
